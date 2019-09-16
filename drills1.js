9a3c34cee4514c001a537002a94b59cfc861346f
zkhin <arandomname@gmail.com>
2019-09-16 14:32:19 -0400

    second commit

--- drills1.js
+++ drills1.js
@@ -1,1 +1,17 @@
-console.log('Hi my name is zayar I am 30 years old');
+
+function getYearOfBirth(age){
+    if (age < 0) {
+        throw new Error('Age can not be negative');
+    }
+    const yearOfBirth = 2019 - age;
+    return yearOfBirth;
+}
+
+function createGreeting(name, age){
+    const yearOfBirth = getYearOfBirth(age);
+    return `I was born in ${yearOfBirth}`;
+}
+
+const greeting1 = createGreeting();
+
+console.log(greeting1);
