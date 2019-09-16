3de48e526b3237ba5791f75d5b250259eb6b1794
zkhin <arandomname@gmail.com>
2019-09-16 15:02:44 -0400

    working

--- drills1.js
+++ drills1.js
@@ -1,17 +1,25 @@
 
 function getYearOfBirth(age){
-    if (age < 0) {
-        throw new Error('Age can not be negative');
-    }
-    const yearOfBirth = 2019 - age;
-    return yearOfBirth;
+    return 2019 - age;
 }
 
 function createGreeting(name, age){
+    if (name === undefined || age === undefined) {
+        throw new Error('name/age not defined');
+    }
+    if (typeof age !== 'number') {
+        throw new Error('age must be a number');
+    }
+    if (age < 0) {
+        throw new Error('age cannot be negative');
+    }
     const yearOfBirth = getYearOfBirth(age);
-    return `I was born in ${yearOfBirth}`;
+    return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
-const greeting1 = createGreeting();
-
-console.log(greeting1);
+try {
+    const greeting1 = createGreeting('Bob', 35);
+    console.log(greeting1);
+} catch(e) {
+    console.error(e.message);
+}
